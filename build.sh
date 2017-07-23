0afd2357ce4e8a252f21c1d807839a0f40cc7a2b
mcaliman <mcaliman@gmail.com>
2017-07-23 17:55:32 +0200

    update

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 # alt-gr+ì is ~
 #
 rm *.*~
+git add --all .
+git commit -m "update"
+git status
 #
 bundle exec jekyll build
--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bundle exec jekyll build
 git add --all .
-git commit -m "update blog_v2"
+git commit -m "update blog_v2017"
 git push origin master
